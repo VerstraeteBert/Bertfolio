5606691793887094ea88f434b09f717b350cbfca
VerstraeteBert <bert-verstraete@hotmail.com>
2018-12-09 00:28:08 +0100

    Sticky footer implementation

--- cms1/footer.php
+++ cms1/footer.php
@@ -1,16 +1,17 @@
 <?php
  /* Main Footer Template */
 ?>
-
+<?php wp_footer(); ?>
+</main>
 <footer class="footer">
-    <div class="footer-text">made with love and mint tea</div>
+    <div class="footer-text">made with ❤️ and mint 🍵</div>
     <div class="social-icons">
        <!-- TODO Dynamic footer icons -->
         <a href="https://github.com/VerstraeteBert">
             <img height="24" width="24" src="<?php bloginfo('template_url') ?>/images/github.svg"/>
         </a>
     </div>
+
 </footer>
-<?php wp_footer(); ?>
 </body>
 </html>
