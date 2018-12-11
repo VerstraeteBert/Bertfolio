002a0b1c51a9b93ea45b3c59be9526db4da4fd1d
VerstraeteBert <bert-verstraete@hotmail.com>
2018-12-11 15:16:41 +0100

    not automatically inserting seperator

--- cms1/content-single.php
+++ cms1/content-single.php
@@ -1,7 +1,6 @@
 <div class="post-full">
     <h1><?php the_title(); ?></h1>
     <div class="post-full-date"><?php the_date(); ?></div>
-    <hr class="hr hr-half">
     <?php the_content(); ?>
     <hr class="hr hr-half">
 </div>
