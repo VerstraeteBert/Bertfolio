0795a9198b088ebdd184b382c62331439d836952
VerstraeteBert <bert-verstraete@hotmail.com>
2018-12-11 15:02:00 +0100

    Adding valid index.php as fallback

--- cms1/index.php
+++ cms1/index.php
@@ -1,1 +1,13 @@
 <?php get_header(); ?>
+
+    <div class="content-wrapper section" id="content">
+    <?php
+        if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+        get_template_part( 'content-single', get_post_format());
+
+        endwhile; endif;
+    ?>
+    </div>
+
+<?php get_footer(); ?>
