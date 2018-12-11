81cdaa71d77d88e3efe5ddb816a0791dc6959ca4
VerstraeteBert <bert-verstraete@hotmail.com>
2018-12-11 16:41:52 +0100

    Added alt tag to work showcase image

--- cms1/archive-work.php
+++ cms1/archive-work.php
@@ -4,29 +4,30 @@ get_header();
 ?>
     <div class="section" id="content">
         <div class="wrap">
-        <div class="flex-wrapper">
-        <?php
-            $args = array( 'post_type' => 'work', 'posts_per_page' => 20 );
-            $loop = new WP_Query( $args );
-            while ( $loop->have_posts() ) : $loop->the_post();
+            <div class="flex-wrapper">
+            <?php
+                $args = array( 'post_type' => 'work', 'posts_per_page' => 20 );
+                $loop = new WP_Query( $args );
+                while ( $loop->have_posts() ) : $loop->the_post();
+                ?>
+                <div class="work-teaser">
+                    <div class="work-thumbnail">
+                        <a class="work-link-no-border" href="<?php the_permalink(); ?>">
+                            <img class="work-screenshot" alt="thumbnail for work showcase" src="<?php the_post_thumbnail_url(); ?>">
+                        </a>
+                    </div>
+                    <div class="work-description top-bottom-margin">
+                        <a class="work-link-no-border" href="<?php the_permalink(); ?>">
+                            <h2 class="work-title"><?php the_title(); ?></h2>
+                        </a>
+                            <?php the_excerpt(); ?>
+                            <a href="<?php the_permalink(); ?>">Read more</a>
+                    </div>
+                </div>
+            <?php
+                endwhile;
             ?>
-            <div class="work-teaser">
-                <div class="work-thumbnail">
-                    <a class="work-link-no-border" href="<?php the_permalink(); ?>">
-                        <img class="work-screenshot" src="<?php the_post_thumbnail_url(); ?>">
-                    </a>
-                </div>
-                <div class="work-description top-bottom-margin">
-                    <a class="work-link-no-border" href="<?php the_permalink(); ?>">
-                        <h2 class="work-title"><?php the_title(); ?></h2>
-                    </a>
-                        <?php the_excerpt(); ?>
-                        <a href="<?php the_permalink(); ?>">Read more</a>
-                </div>
             </div>
-        <?php
-            endwhile;
-        ?>
         </div>
     </div>
 <?php get_footer(); ?>
