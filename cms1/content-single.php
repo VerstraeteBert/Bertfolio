ee14f735c85d70622ba586889ad99baae91335cf
VerstraeteBert <bert-verstraete@hotmail.com>
2018-12-11 10:00:27 +0100

    Blog post styling tweaks

--- cms1/content-single.php
+++ cms1/content-single.php
@@ -1,6 +1,7 @@
 <div class="post-full">
     <h1><?php the_title(); ?></h1>
     <div class="post-full-date"><?php the_date(); ?></div>
+    <hr class="hr hr-half">
     <?php the_content(); ?>
     <hr class="hr hr-half">
 </div>
