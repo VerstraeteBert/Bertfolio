0abb9d6f25d7bab308767a74cf8b823e49af8d68
VerstraeteBert <bert-verstraete@hotmail.com>
2018-12-14 10:13:40 +0100

    Rename cms1 folder -> WP

--- cms1/content-single.php
+++
@@ -1,6 +0,0 @@
-<div class="post-full">
-    <h1><?php the_title(); ?></h1>
-    <div class="post-full-date"><?php the_date(); ?></div>
-    <?php the_content(); ?>
-    <hr class="hr hr-half">
-</div>
--- cms1/content.php
+++
@@ -1,4 +0,0 @@
-<div class="post-summary">
-    <div class="post-summary-date"><?php the_date(); ?></div>
-    <a class="post-summary-title" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-</div>
